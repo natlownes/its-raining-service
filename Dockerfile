fbccfce7fa75730597178fdd14b317b30fab0ab1
nat <nat.lownes@gmail.com>
2018-02-19 23:14:12 -0500

    add /ai endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM narf/its-raining-various-things:2.2.0
+FROM narf/its-raining-various-things@sha256:9457186502a19907af844d5e134f78ab02c64cc6e425a57e200121a7782ed2ce
 
 COPY app /app
 
--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func handleSing(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	resp, err := audio(body)
+	resp, err := audio("raining", body)
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -35,7 +35,7 @@ func handleSing(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("content-type", "audio/mpeg")
 
-	log.Println("[app] phrase: ", body)
+	log.Println("[app:raining] phrase: ", body)
 
 	if _, err := w.Write(resp); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -44,8 +44,39 @@ func handleSing(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func audio(phrase string) ([]byte, error) {
-	cmd := exec.Command(COMMAND)
+func handleIverson(w http.ResponseWriter, r *http.Request) {
+	body := r.FormValue("w")
+	if len(body) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if len(body) > LENGTH_LIMIT {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, fmt.Sprintf("characters limited to %v", LENGTH_LIMIT))
+		return
+	}
+
+	resp, err := audio("iverson", body)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("[app] err: ", err)
+		return
+	}
+
+	w.Header().Set("content-type", "audio/mpeg")
+
+	log.Println("[app:iverson] phrase: ", body)
+
+	if _, err := w.Write(resp); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("[app] err: ", err)
+		return
+	}
+}
+
+func audio(theme string, phrase string) ([]byte, error) {
+	cmd := exec.Command(COMMAND, "--", theme)
 	cmd.Stdin = strings.NewReader(phrase)
 	return cmd.Output()
 }
@@ -58,6 +89,8 @@ func handlePing(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/s", handleSing)
 	http.HandleFunc("🎶", handleSing)
+	http.HandleFunc("/raining", handleSing)
+	http.HandleFunc("/ai", handleIverson)
 	http.HandleFunc("/varz/ping", handlePing)
 	http.ListenAndServe(":8080", nil)
 }
